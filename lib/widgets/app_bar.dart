b7016ed7d48a39ee5c1917ec3bc370e52283bf3e
Fedi Hamdi <fedihamdi97@gmail.com>
2022-04-26 00:26:51 +0100

    delete device id on logout

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables, must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:newapp/config/config.dart';
 import 'package:newapp/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   var back;
@@ -12,6 +14,17 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       : super(key: key);
   logout(context) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    var user = prefs.getString('user');
+    var userJson = json.decode(user!);
+    var token = prefs.getString('token');
+    var from = userJson['_id'];
+    Uri url = Uri.parse("$apiUrl/api/employee/removeDeviceId/$from");
+    var res = await http.put(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'jwt $token',
+        });
+
     // clear shared preferences
     prefs.clear();
     // navigate to login screen
